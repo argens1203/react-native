7a7484721a98ef9eebf48a2d98884aebbca59f24
Argens <ng.argens@gmail.com>
2022-02-08 20:33:17 +0800

    wip: calendar library

--- screens/test-tab/index.tsx
+++ screens/test-tab/index.tsx
@@ -1,13 +1,30 @@
+import { useState } from 'react';
 import { StyleSheet } from 'react-native';
+import { Button } from 'react-native';
+import RNCalendarEvents from "react-native-calendar-events";
 
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
 import { RootTabScreenProps } from '../../types';
 
 export default function TestTabScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+  const [text, setText] = useState('Unclicked');
+
+  const checkPermission = async () => {
+    const result = await RNCalendarEvents.checkPermissions(false);
+    setText(JSON.stringify(result));
+  }
+
+  const requestPermission = async () => {
+    const result = await RNCalendarEvents.requestPermissions(false);
+    setText(JSON.stringify(result));
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Three</Text>
+      <Button onPress={checkPermission} title={'Check Permission'}/>
+      <Button onPress={requestPermission} title={'Request Permission'}/>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
     </View>
   );
