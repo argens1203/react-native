1a4bd353d636597ddfc919373a5fda5b25aa6e80
Argens <ng.argens@gmail.com>
2022-02-08 23:34:14 +0800

    feat: use expo-calendar

--- screens/test-tab/index.tsx
+++ screens/test-tab/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { Button } from 'react-native';
-import RNCalendarEvents from "react-native-calendar-events";
+import * as Calendar from 'expo-calendar';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
@@ -9,23 +9,33 @@ import { RootTabScreenProps } from '../../types';
 
 export default function TestTabScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   const [text, setText] = useState('Unclicked');
-
-  const checkPermission = async () => {
-    const result = await RNCalendarEvents.checkPermissions(false);
-    setText(JSON.stringify(result));
-  }
+  const [error, setError] = useState('No Error Yet');
 
   const requestPermission = async () => {
-    const result = await RNCalendarEvents.requestPermissions(false);
-    setText(JSON.stringify(result));
+    try {
+      const result = await Calendar.requestCalendarPermissionsAsync();
+      setText(JSON.stringify(result));
+    } catch (e){
+      setError (e.message);
+    }
+  }
+
+  const getCalendarEvents = async () => {
+    try {
+      const result = await Calendar.getCalendarsAsync(Calendar.EntityTypes.EVENT);
+      setText(JSON.stringify(result[0]));
+    } catch (e){
+      setError (e.message);
+    }
   }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Three</Text>
-      <Button onPress={checkPermission} title={'Check Permission'}/>
+      <Text style={styles.title}>{text}</Text>
       <Button onPress={requestPermission} title={'Request Permission'}/>
+      <Button onPress={getCalendarEvents} title={'Get Calendars Event'}/>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text style={styles.title}>{error}</Text>
     </View>
   );
 }
@@ -39,6 +49,10 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+  },
+  error: {
+    fontSize: 20,
+    color: 'red',
   },
   separator: {
     marginVertical: 30,
