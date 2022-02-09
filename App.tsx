fca236b42aafeea9cd0b030595c460d49dfad33c
Argens <ng.argens@gmail.com>
2022-02-09 10:06:48 +0800

    feat: ota updates

--- App.tsx
+++ App.tsx
@@ -3,13 +3,15 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
+import { useExpoUpdate } from './hooks/useExpoUpdate';
 import Navigation from './navigation';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+  const {isLoading, isLatest} = useExpoUpdate();
 
-  if (!isLoadingComplete) {
+  if (!isLoadingComplete || isLoading) {
     return null;
   } else {
     return (
